43c996aeb1d5047fdab6d21bdb52d34666440b7e
Eduardaskuliesa <eduardas2000@mail.ru>
2025-06-25 17:04:50 +0300

    feat: Created update and repalce functions in google driver

--- src/controllers/googleController.t/index.ts
+++ src/controllers/googleController.t/index.ts
@@ -2,11 +2,13 @@ import { createInExistingFolder } from "./createInExistingFolder"
 import { createNewFoldersAndFiles } from "./createNewFolderAndFiles"
 import { getFolderFilesContent } from "./getFolderFilesContent"
 import { getFoldersWithFiles } from "./getFoldersWithFiles"
+import { updateFileContent } from "./updateFileContent"
 
 
 export const googleController = {
     getFoldersWithFiles,
     getFolderFilesContent,
     createNewFoldersAndFiles,
-    createInExistingFolder
+    createInExistingFolder,
+    updateFileContent
 }
--- src/routes/googleRoutes.ts
+++ src/routes/googleRoutes.ts
@@ -5,6 +5,7 @@ const router = Router();
 
 export const googleRoutes = async (server: Express) => {
     server.post('/api/create-in-folder', googleController.createInExistingFolder);
+    server.post('/api/update-file', googleController.updateFileContent)
     server.post('/api/create-new', googleController.createNewFoldersAndFiles)
     server.get('/api/folders', googleController.getFoldersWithFiles)
     server.post('/api/content', googleController.getFolderFilesContent)
